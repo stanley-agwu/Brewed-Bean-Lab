c3c2875bb7952c292d6c7a4ff5a375c328c47146
stanley-agwu <stanleyagwu2016@gmail.com>
2025-12-08 15:12:02 +0100

    feat(backend): Added base route

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import type { Request, Response } from 'express';
+
+const router: Router = Router()
+
+router.get('/', (req: Request, res: Response) => res.send('Server running'));
+
+export default router;
